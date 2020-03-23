cbff855555bacc492ea92592a19f322b1a0c1558
lisiatguiguu <wangfuying726@163.com>
2020-03-23 18:04:41 +0800

    新

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -3,6 +3,6 @@ package com.atguigu.bean;
 public class Love {
 public static void main(String[] args) {
 	System.out.println("我喜欢你，丽丝！--张三1111");
-	System.out.println("are you handsome at guigu？---lisi2222");
+	System.out.println("are you handsome at guigu？---lisi2222新");
 }
 }
