1184ca5a71db555cf5a603b5c6ea12e79628346a
lisiatguiguu <wangfuying726@163.com>
2020-03-23 13:08:36 +0800

    2222

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -3,6 +3,6 @@ package com.atguigu.bean;
 public class Love {
 public static void main(String[] args) {
 	System.out.println("我喜欢你，丽丝！--张三");
-	System.out.println("are you handsome at guigu？---lisi");
+	System.out.println("are you handsome at guigu？---lisi2222");
 }
 }
