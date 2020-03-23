5323d53216222ba8e7419d1b1c6b404f1df1c65a
zhangsanatguiguu <782699818@qq.com>
2020-03-23 13:07:44 +0800

    1111

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -2,7 +2,7 @@ package com.atguigu.bean;
 
 public class Love {
 public static void main(String[] args) {
-	System.out.println("我喜欢你，丽丝！--张三");
+	System.out.println("我喜欢你，丽丝！--张三1111");
 	System.out.println("are you handsome at guigu？---lisi");
 }
 }
